3258689d05780f035abf4af092688f4093ef9a1e
ruhith <ruhithudakara77@gmail.com>
2022-05-24 19:24:31 +0530

    initial commit

---
+++ src/components/CardView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"></nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex flex-row flex-wrap py-4">
+      <div class="sticky top-0 p-4 w-full">
+        <NavBar />
+      </div>
+      <div role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+        <!-- content area -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+  },
+};
+</script>
